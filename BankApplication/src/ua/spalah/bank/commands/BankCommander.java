8d85e3d5ea26c9cff23535288153c161d8e22a8c
nikolaev-dmitriy <nikolaev.dmitriy.1712@gmail.com>
2017-01-12 18:18:29 +0200

    GetBankInfoCommand

--- BankApplication/src/ua/spalah/bank/commands/BankCommander.java
+++ BankApplication/src/ua/spalah/bank/commands/BankCommander.java
@@ -6,8 +6,10 @@ import ua.spalah.bank.models.accounts.CheckingAccount;
 import ua.spalah.bank.models.accounts.SavingAccount;
 import ua.spalah.bank.models.type.Gender;
 import ua.spalah.bank.services.AccountService;
+import ua.spalah.bank.services.BankReportService;
 import ua.spalah.bank.services.ClientService;
 import ua.spalah.bank.services.impl.AccountServiceImpl;
+import ua.spalah.bank.services.impl.BankReportServiceImpl;
 import ua.spalah.bank.services.impl.ClientServiceImpl;
 
 import java.util.InputMismatchException;
@@ -35,7 +37,7 @@ public class BankCommander {
 
             ClientService clientService = new ClientServiceImpl();
             AccountService accountService = new AccountServiceImpl();
-
+            BankReportService bankReportService = new BankReportServiceImpl();
             Bank bank = new Bank();
 
             Client dima = new Client("Dima", Gender.MALE);
---
+++ BankApplication/src/ua/spalah/bank/commands/GetBankInfoCommand.java
@@ -0,0 +1,28 @@
+package ua.spalah.bank.commands;
+
+import ua.spalah.bank.services.BankReportService;
+
+/**
+ * Created by Man on 12.01.2017.
+ */
+public class GetBankInfoCommand implements Command {
+    private final BankReportService bankReportService;
+
+    public GetBankInfoCommand(BankReportService bankReportService) {
+        this.bankReportService = bankReportService;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Clients: "+bankReportService.getClientsSortedByName(BankCommander.currentBank));
+        System.out.println("Number of clients: "+bankReportService.getNumberOfClients(BankCommander.currentBank));
+        System.out.println("Number of accounts: "+bankReportService.getNumberOfAccounts(BankCommander.currentBank));
+        System.out.println("Total bank's savings: "+bankReportService.getTotalAccountSum(BankCommander.currentBank));
+        System.out.println("Total bank's credits: "+bankReportService.getBankCreditSum(BankCommander.currentBank));
+    }
+
+    @Override
+    public String getCommandInfo() {
+        return "Print info about bank";
+    }
+}
